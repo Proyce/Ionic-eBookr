3076cfd6494ce804e83e33fcb2841f20224416df
Prosper Nnadi <prospernnadi3@gmail.com>
2023-06-09 23:49:18 +0100

    Modified lazy loading for the page routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,30 +20,6 @@ const routes: Routes = [
     loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
   },
   {
-    path: 'discover',
-    loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./places/offers/offers.module').then( m => m.OffersPageModule)
-  },
-  {
-    path: 'new-offer',
-    loadChildren: () => import('./places/offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
-  },
-  {
-    path: 'edit-0ffer',
-    loadChildren: () => import('./places/offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
-  },
-  {
-    path: 'place-detail',
-    loadChildren: () => import('./places/discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
-  },
-  {
-    path: 'offer-bookings',
-    loadChildren: () => import('./places/offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
-  },
-  {
     path: 'bookings',
     loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   },
